da6c67f5077d8906000f1451689f23e5613fcd4f
Euge Job <tessov.evgueni@gmail.com>
2014-04-25 11:21:50 -0300

    Proyectos actualizados

--- PruebaTestUnitario/src/PruebaTestUnitario.c
+++ PruebaTestUnitario/src/PruebaTestUnitario.c
@@ -10,13 +10,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <CUnit/CUnit.h>
 #include <CUnit/TestRun.h>
+#include <CUnit/Basic.h>
+#include <assert.h>
 
 int suma(int x, int y);
+int maxi(int i1, int i2);
+void test_maxi(void);
 
 int main(void)
 {
-	printf("La suma de 2 y 3 es: %d", suma(2, 3));
+	//printf("La suma de 2 y 3 es: %d", suma(2, 3));
+
+	//printf("%d", maxi(1,2));
+
+	test_maxi();
 
 	return EXIT_SUCCESS;
 }
@@ -25,6 +34,20 @@ int suma(int x, int y)
 {
 	return (x + y);
 }
+
+
+int maxi(int i1, int i2)
+{
+  return (i1 > i2) ? i1 : i2;
+}
+
+void test_maxi(void)
+{
+  CU_ASSERT_EQUAL(suma(1,2), 3);
+  //CU_ASSERT(maxi(0,-2) == 0);
+  //CU_ASSERT(maxi(2,2) == 2);
+}
+
 
 
 
